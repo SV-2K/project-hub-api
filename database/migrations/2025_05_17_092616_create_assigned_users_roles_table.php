c312f2a7a307d5a82309dd6d46a578bd10add348
SV2K <savva7446@gmail.com>
2025-05-17 16:59:20 +0700

    Add DB migrations

---
+++ database/migrations/2025_05_17_092616_create_assigned_users_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assigned_users_roles', function (Blueprint $table) {
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('project_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('role_id')
+                ->constrained()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assigned_users_roles');
+    }
+};
