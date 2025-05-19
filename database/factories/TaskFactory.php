4a58420b99c42a5073b69b1ef987c8f295941d4b
SV2K <savva7446@gmail.com>
2025-05-19 12:32:56 +0700

    Add factories

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'project_id' => rand(1, 5),
+            'title' => fake()->sentence(3),
+            'description' => fake()->text(),
+            'status' => Arr::random(['in progress', 'completed', 'canceled']),
+            'priority' => Arr::random(['low', 'medium', 'high']),
+            'deadline' => fake()->dateTime()
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Project;
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
+        Project::factory(5)->create();
+        Task::factory(25)->create();
+        Comment::factory(50)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('roles')->insert([
+            ['role' => 'manager'],
+            ['role' => 'executor']
         ]);
+
+        $pivotTableData = [];
+
+        for ($i = 0; $i < 20; $i++) {
+            $pivotTableData[] = [
+                'user_id' => rand(1, 10),
+                'project_id' => rand(1, 5),
+                'role_id' => rand(1, 2)
+            ];
+        }
+
+        DB::table('assigned_users_roles')->insert($pivotTableData);
     }
 }
